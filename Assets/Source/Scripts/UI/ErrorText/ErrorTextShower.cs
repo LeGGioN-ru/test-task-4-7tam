c49ee523a763b3e7b190c8d41049e8d6f01c4218
LeGGioN-ru <nikita90017@gmail.com>
2023-05-07 17:57:18 +0300

    Add error text shower

---
+++ Assets/Source/Scripts/UI/ErrorText/ErrorTextShower.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using DG.Tweening;
+
+public class ErrorTextShower : MonoBehaviour
+{
+    public static ErrorTextShower Instance;
+
+    [SerializeField] private TMP_Text _text;
+    [SerializeField] private Color _textColor;
+    [SerializeField] private float _fadeAwayDelay;
+    [SerializeField] private float _fadeAwayDuration;
+
+    private readonly float _zeroAlpha = 0;
+    private Tween _fadingAway;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Execute(string errorText)
+    {
+        if (_fadingAway.IsActive())
+            _fadingAway.Kill();
+
+        _text.text = errorText;
+        _text.color = _textColor;
+
+        _fadingAway = _text.DOFade(_zeroAlpha, _fadeAwayDuration).SetDelay(_fadeAwayDelay);
+    }
+}
